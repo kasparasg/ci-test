7912745421a9ec438e157519dd917c5fe18872cb
Kasparas Galdikas <kasparas.galdikas@rakuten.com>
2017-06-30 14:10:15 +0100

    Setp amendments

--- scripts/private/test-all.sh
+++ scripts/private/test-all.sh
@@ -2,18 +2,6 @@
 
 set -ex
 
-cleanUp () {
-  kill $WEBSERVER_PID
-  git checkout -f $BRANCH
-}
-
-trap cleanUp EXIT
-
-ROOT_DIR=`dirname $0`/../..
-
-cd $ROOT_DIR
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
 # Ensure that all the dependencies are there
 npm install
 
@@ -24,12 +12,5 @@ npm run update-webdriver
 node_modules/.bin/http-server ./app -a localhost -p 8000 -c-1 --silent &
 WEBSERVER_PID=$!
 
-# Run the unit and e2e tests
-# (Steps 0 and 1 do not have tests)
-for i in $(seq 2 14)
-do
-  git checkout -f step-$i
-
-  node_modules/.bin/karma start karma.conf.js --single-run
-  node_modules/.bin/protractor e2e-tests/protractor.conf.js --directConnect
-done
+node_modules/.bin/karma start karma.conf.js --single-run
+node_modules/.bin/protractor e2e-tests/protractor.conf.js --directConnect
